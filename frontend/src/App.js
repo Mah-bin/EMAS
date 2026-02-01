b45529ff18f5f2fba33870ade1482442d21e34eb
Mahel B <mahelbinoy@gmail.com>
2026-02-01 06:27:55 +0530

    changed link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   // --- 1. CITY SELECTION STATE ---
